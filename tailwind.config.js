91300712ba60e1060accb5ffc5899a33888905ea
Nisaayu <ayy515951@gmail.com>
2024-11-10 21:31:51 +0700

    Melakukan konfigurasi ulang agar styling bisa terbaca oleh localhost dan bisa di running

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        md: '0 0px 6px 0px rgba(0, 0, 0 , 0.1)',
+      },
+      scale: {
+        102: '1.02',
+      },
+    },
   },
   plugins: [],
-}
-
+};
